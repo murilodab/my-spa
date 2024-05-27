b5026711ea32407a2a905ff57f0e7ca3ef6886f9
Murilo Barbosa <muriloab96@gmail.com>
2024-05-27 18:52:28 -0300

    adicionando routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { BlogListComponent } from './blog-list/blog-list.component';
+import { BlogPostComponent } from './blog-post/blog-post.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: BlogListComponent},
+    { path: 'blog-post-component', component: BlogPostComponent}
+];
