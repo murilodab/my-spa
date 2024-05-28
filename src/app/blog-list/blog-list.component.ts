0706a43ab8507e69310b52ad3b75df29c8fd67eb
Murilo Barbosa <muriloab96@gmail.com>
2024-05-27 22:19:14 -0300

    commit

--- src/app/blog-list/blog-list.component.ts
+++ src/app/blog-list/blog-list.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { BlogPost } from '../models/blog-post-model';
+import { BlogPostComponent } from '../blog-post/blog-post.component';
+
 
 @Component({
   selector: 'app-blog-list',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, BlogPostComponent],
   templateUrl: './blog-list.component.html',
   styleUrl: './blog-list.component.css'
 })
+
 export class BlogListComponent {
+
+  blogPost: BlogPost = {
+    id:1,
+    title: "First Post",
+    content: "some content",
+    
+  };
 
 }
--- src/app/blog-post/blog-post.component.ts
+++ src/app/blog-post/blog-post.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-blog-post',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './blog-post.component.html',
   styleUrl: './blog-post.component.css'
 })
